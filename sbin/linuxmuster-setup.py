b093c7db459edb42f3cc875e3f52adca45991b7c
zefanja <npm@zefanjas.de>
2017-02-08 21:31:05 +0700

    * added command line option to skip ipfire ssh setup #2

--- sbin/linuxmuster-setup.py
+++ sbin/linuxmuster-setup.py
@@ -17,11 +17,12 @@ def usage():
     print(' [options] may be:')
     print(' -c <file>, --config=<file> : path to ini file with setup values')
     print(' -u,        --unattended    : unattended mode, do not ask questions')
+    print(' -s,        --skip-fw-setup : skip firewall SSH setup')
     print(' -h,        --help          : print this help')
 
 # get cli args
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "c:hu", ["config=", "help", "unattended"])
+    opts, args = getopt.getopt(sys.argv[1:], "c:hsu", ["config=", "help", "unattended", "skip-fw-setup"])
 except getopt.GetoptError as err:
     # print help information and exit:
     print(err) # will print something like "option -a not recognized"
@@ -30,11 +31,14 @@ except getopt.GetoptError as err:
 
 # default values
 unattended = False
+skip_firewall = False
 
 # evaluate options
 for o, a in opts:
     if o == "-u":
         unattended = True
+    if o == "-s":
+        skip_firewall = True
     elif o in ("-c", "--config"):
         if os.path.isfile(a):
             os.system('cp ' + a + ' ' + constants.CUSTOMINI)
@@ -57,6 +61,8 @@ setup_modules.remove('__pycache__')
 setup_modules.sort()
 for f in setup_modules:
     if (unattended == True and f == 'b_dialog.py'):
+        continue
+    if (skip_firewall == True and f == 'f_ssh.py'):
         continue
     m = os.path.splitext(f)[0]
     importlib.import_module(m)
