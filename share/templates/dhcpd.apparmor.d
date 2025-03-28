a116c0e731b931152fc75211651d852fe0957208
Thomas Schmitt <thomas@linuxmuster.net>
2025-03-28 19:55:53 +0100

    fix local apparmor configs for dhcpd and ntpd.

--- share/templates/dhcpd.apparmor.d
+++ share/templates/dhcpd.apparmor.d
@@ -1,9 +1,6 @@
 # /etc/apparmor.d/local/usr.sbin.dhcpd
 #
 # thomas@linuxmuster.net
-# 20200415
+# 20250328
 #
-
-{
-	/usr/lib/linuxmuster/dhcpd-update-samba-dns.py Ux,
-}
+/usr/lib/linuxmuster/dhcpd-update-samba-dns.py Ux,
