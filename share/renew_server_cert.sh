c863a7796bf01e2a5af700bf712df95abf3d70fa
Thomas Schmitt <thomas@linuxmuster.net>
2025-03-30 13:36:34 +0200

    improved certificate renewal script.

--- share/renew_server_cert.sh
+++ share/renew_server_cert.sh
@@ -2,39 +2,48 @@
 #
 # renew self-signed server cert
 # thomas@linuxmuster.net
-# 20250329
+# 20250330
 #
 
 . /usr/share/linuxmuster/defaults.sh || exit 1
 
-DAYS="7300"
+DAYS="7305"
+CACERT_OLD="${CACERT}_old"
+CACERTB64_OLD="${CACERTB64}_old"
+SRVCERT="$SSLDIR/server.cert.pem"
+SRVCERT_OLD="${SRVCERT}_old"
+SRVKEY="$SSLDIR/server.key.pem"
+SRVBUNDLE="$SSLDIR/server.cert.bundle.pem"
+SRVFULLCHAIN="$SSLDIR/server.fullchain.pem"
+FWCERT="$SSLDIR/firewall.cert.pem"
+FWCERT_OLD="${FWCERT}_old"
+FWCERTB64="${FWCERT}.b64"
+FWCERTB64_OLD="${FWCERTB64}_old"
+FWKEY="$SSLDIR/firewall.key.pem"
 
 # ca
 echo "Renewing $(basename $CACERT) with $DAYS days."
-CACERT_OLD="${CACERT}_old"
 mv "$CACERT" "$CACERT_OLD"
 openssl x509 -days "$DAYS" -in "$CACERT_OLD" -signkey "$CAKEY" -passin pass:"$(cat "$CAKEYSECRET")" -out "$CACERT"
 # export key for firewall without password
 openssl rsa -in "$CAKEY" --passin pass:"$(cat "$CAKEYSECRET")" --out "${CAKEY}.extracted"
+mv "$CACERTB64" "$CACERTB64_OLD"
 base64 "$CACERT" | sed '{:q;N;s/\n//g;t q}' > "$CACERTB64"
 
 # server
-SRVCERT="$SSLDIR/server.cert.pem"
 echo "Renewing $(basename $SRVCERT) with $DAYS days."
-SRVCERT_OLD="${SRVCERT}_old"
-SRVKEY="$SSLDIR/server.key.pem"
-SRVBUNDLE="$SSLDIR/server.cert.bundle.pem"
 mv "$SRVCERT" "$SRVCERT_OLD"
 openssl x509 -days "$DAYS" -in "$SRVCERT_OLD" -signkey "$SRVKEY" -out "$SRVCERT"
-cat "$SRVCERT" "$SRVKEY" > "$SRVBUNDLE"
+cat "$SRVCERT" "$SRVKEY" > "$SRVFULLCHAIN"
+[ -e "$SRVBUNDLE" ] && mv "$SRVBUNDLE" "${SRVBUNDLE_old}"
 
 # firewall
-if [ ! -e "${FWFULLCHAIN}_old" ]; then
-  echo "Moving obsolete firewall cert files."
-  for i in "$SSLDIR"/firewall.*; do
-    mv "$i" "${i}_old"
-  done
-fi
+echo "Renewing $(basename $FWCERT) with $DAYS days."
+mv "$FWCERT" "$FWCERT_OLD"
+openssl x509 -days "$DAYS" -in "$FWCERT_OLD" -signkey "$FWKEY" -out "$FWCERT"
+cat "$FWCERT" "$FWKEY" > "$FWFULLCHAIN"
+mv "$FWCERTB64" "$FWCERTB64_OLD"
+base64 "$FWCERT" | sed '{:q;N;s/\n//g;t q}' > "$FWCERTB64"
 
 chgrp ssl-cert "$SSLDIR"/*
 chmod 600 "$SSLDIR"/*key.*
