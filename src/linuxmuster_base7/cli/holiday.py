5b5a1267edb92a16ff6e5dd6d2015719cb12e761
HappyBasher <thomas@linuxmuster.net>
2025-11-13 08:48:22 +0100

    Fix holiday.py: Replace broken wrapper with actual implementation
    - Removed circular wrapper code that tried to load itself
    - Restored original implementation from sbin/linuxmuster-holiday
    - Updated date to 20251113
    - Added proper main() function as entry point
    - Removed unused os import and sys.path manipulation
    - Removed unused exception variable 'e' from except clauses

--- src/linuxmuster_base7/cli/holiday.py
+++ src/linuxmuster_base7/cli/holiday.py
@@ -1,31 +1,122 @@
 #!/usr/bin/python3
 #
-# CLI entry point wrapper for linuxmuster-holiday
-# Auto-generated for Debian Python Policy compliance
-# 20251111
+# linuxmuster-holiday
+# thomas@linuxmuster.net
+# 20251113
 #
 
-import sys
-import os
+"""
+Simple script to test if today is holiday, based on the configuration file
+/etc/linuxmuster/sophomorix/SCHOOL/holidays.yml.
 
-# Add linuxmuster-common to path for environment module
-sys.path.insert(0, '/usr/lib/linuxmuster')
+Structure of the configuration file (YAML):
+holiday_name1:
+  start: "dd.mm.yyyy"
+  end: "dd.mm.yyyy"
+holiday_name2:
+  start: "dd.mm.yyyy"
+  end: "dd.mm.yyyy"
+"""
+
+import getopt
+import sys
+import yaml
+
+from datetime import date
+
+
+class Holiday:
+    def __init__(self, name, start, end):
+        """
+        Holiday object.
+        :param name: Name of holiday
+        :type name: string
+        :param start: Start date as dd.mm.yyyy
+        :type start: string
+        :param end: End as dd.mm.yyyy
+        :type end: string
+        """
+
+        self.name = name
+        self.start = date(*map(int, start.split('.')[::-1]))
+        self.end = date(*map(int, end.split('.')[::-1]))
+
+    def __str__(self):
+        return f"{self.name} ({self.start} - {self.end})"
+
+
+def TestToday(config):
+    """
+    Method to get all holidays stored in configuration file and test if the current day is in holiday.
+    """
+    today = date.today()
+
+    try:
+        with open(config, 'r') as f:
+            holidays_dict = yaml.load(f.read(), Loader=yaml.SafeLoader)
+    except FileNotFoundError:
+        print(f"The file {config} does not seem to exists, you need to configure it first with the Webui.")
+        return None
+    except yaml.scanner.ScannerError:
+        print(f"The file {config} does not seem to respect yaml standards, you need to verify it.")
+        return None
+
+    holidays = []
+    try:
+        for name, dates in holidays_dict.items():
+            holidays.append(Holiday(name, dates['start'], dates['end']))
+
+        for holiday in holidays:
+            if holiday.start <= today <= holiday.end:
+                return holiday
+    except AttributeError:
+        print(f"The file {config} seems to be empty, you need to configure it first with the Webui.")
+
+    return None
+
+
+def usage():
+    print("""
+Usage: linuxmuster-holiday [options]
+Options:
+    -s, --school <file> - Specify school to process. Process 'default-school' if nothing is specified.
+    -h, --help          - Show this help
+    """)
+
 
 def main():
-    """Main entry point wrapper."""
-    # Import and execute the original script logic
-    original_script = os.path.join('/usr/sbin', 'linuxmuster-holiday')
-    
-    # For now, we'll import the functions and re-execute
-    # This will be properly refactored in a later step
-    import importlib.util
-    spec = importlib.util.spec_from_file_location("cli_module", original_script)
-    if spec and spec.loader:
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
+    """Test if today is a holiday."""
+    school = 'default-school'
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            'hs:',
+            ['help', 'school=']
+        )
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-s", "--school"):
+            school = a
+
+    config = f"/etc/linuxmuster/sophomorix/{school}/holidays.yml"
+
+    isTodayInHoliday = TestToday(config)
+
+    if isTodayInHoliday is None:
+        print("Today is not holiday")
+        sys.exit()
     else:
-        print(f"Error: Could not load {original_script}", file=sys.stderr)
-        sys.exit(1)
+        print(f"Today is holiday in: {isTodayInHoliday}")
+        sys.exit(-1)
+
 
 if __name__ == '__main__':
     main()
